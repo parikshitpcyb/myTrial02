e49aec966b7c8086770801fe7d255c23fb369948
Parikshit Parihar <41181121+parikshitpcyb@users.noreply.github.com>
2020-08-31 16:02:55 +0530

    AddFiles
    Add Files

---
+++ src/main/java/myTrial02/myTrial02/App.java
@@ -0,0 +1,13 @@
+package myTrial02.myTrial02;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/myTrial02/myTrial02/myJavaClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package myTrial02.myTrial02;
+
+/**
+ * @author parikshitp
+ *
+ */
+public class myJavaClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
